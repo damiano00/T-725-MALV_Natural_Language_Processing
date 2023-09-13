3c6fa137ab13b4ee9e447210e22fb16b10ece2ca
damiano00 <pasquini.damiano00@gmail.com>
2023-09-13 11:15:04 +0000

    done: trigram_language_model.sh awk command

--- assignment_1/languageModeling/trigram_language_model.sh
+++ assignment_1/languageModeling/trigram_language_model.sh
@@ -5,11 +5,9 @@
 # in order to construct the language model you need a file with one token (word) per line without any empty
 # lines. Use awk for this purpose. Show the awk command that you use for constructing this file, eng.tok.
 
-#TODO: awk command
 awk '{
-    for (i=1; i<=NF; i+=2) {
-        if (i+1 <= NF ) {
-            printf "%s\n",$i
-        }
+    for(i=1; i<=NF; i+=2) {
+        printf "%s\n",$i
     }
 }' eng.sent > eng.tok
+
