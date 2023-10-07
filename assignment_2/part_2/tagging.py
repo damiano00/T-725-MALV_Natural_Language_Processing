9ac272e2c9b6729284ab1a4c7e0ac7338cc0bcc9
damiano00 <pasquini.damiano00@gmail.com>
2023-10-07 13:48:55 +0000

    DONE: point 2 code refactoring

--- assignment_2/part_2/tagging.py
+++ assignment_2/part_2/tagging.py
@@ -1,51 +1,63 @@
 import nltk
-import numpy as np
-from nltk import pos_tag, word_tokenize
 from nltk.corpus import treebank
+
 nltk.download('treebank')
+
+taggers = [nltk.AffixTagger, nltk.UnigramTagger, nltk.BigramTagger, nltk.TrigramTagger]
+tagged_sentences = treebank.tagged_sents()
+training_set = tagged_sentences[:3500]
+test_set = tagged_sentences[3500:]
+
+
+def run_tagger(tagger, train_set, testing_set, backoff_tagger=None):
+    """
+    This function trains a tagger on the training set and evaluates it on the test set
+    :param tagger: tagger from nltk to be used to tag the sentences
+    :param train_set: the training set to train the tagger on
+    :param testing_set: the test set to evaluate the tagger on
+    :param backoff_tagger: the tagger to be used as backoff, if None, no backoff is used
+    :return: the trained tagger
+    """
+    tagger = tagger(train_set, backoff=backoff_tagger)
+    print(tagger.__class__.__name__ +" evaluation:", round(tagger.accuracy(testing_set) * 100, 2), "%")
+    return tagger
+
+
+def evaluate(results, expected):
+    """
+    For each sentence in the test set, this function compares the results of the default tagger with the expected
+    :param results: The results of the default tagger
+    :param expected: The expected results
+    :return: The accuracy of the default tagger
+    """
+    correct = 0
+    total = 0
+    for i in range(len(results)):
+        for j in range(len(results[i])):
+            if results[i][j][1] == expected[i][j][1]:
+                correct += 1
+            total += 1
+    return correct / total
 
 
 def main():
-
     # 2.1
-    tagged_sentences = treebank.tagged_sents()
-    training_set = tagged_sentences[:3500]
-    test_set = tagged_sentences[3500:]
     print("Number of training sentences: ", len(training_set))
     print("Number of test sentences: ", len(test_set), "\n")
     print("First sentence in test corpus: " + "\n" + str(test_set[0]) + "\n")
 
     # 2.2
     print("Tagging accuracies:\n-------------------")
-    affix_tagger = nltk.AffixTagger(training_set)
-    print("Affix tagger: ", round(affix_tagger.accuracy(test_set)*100, 2), "%")
-    unigram_tagger = nltk.UnigramTagger(training_set)
-    print("Unigram tagger: ", round(unigram_tagger.accuracy(test_set)*100, 2), "%")
-    bigram_tagger = nltk.BigramTagger(training_set)
-    print("Bigram tagger: ", round(bigram_tagger.accuracy(test_set)*100, 2), "%")
-    trigram_tagger = nltk.TrigramTagger(training_set)
-    print("Trigram tagger: ", round(trigram_tagger.accuracy(test_set)*100, 2), "%", "\n")
+    for tagger in taggers:
+        run_tagger(tagger, training_set, test_set)
 
     # 2.3
-    print("Tagging accuracies with backoff:\n--------------------------------")
-    backoff_tagger = affix_tagger
-    print("Affix tagger evaluation: ", round(backoff_tagger.accuracy(test_set)*100, 2), "%")
-    backoff_tagger = nltk.UnigramTagger(training_set, backoff=backoff_tagger)
-    print("Unigram tagger evaluation: ", round(backoff_tagger.accuracy(test_set)*100, 2), "%")
-    backoff_tagger = nltk.BigramTagger(training_set, backoff=backoff_tagger)
-    print("Bigram tagger evaluation: ", round(backoff_tagger.accuracy(test_set)*100, 2), "%")
-    backoff_tagger = nltk.TrigramTagger(training_set, backoff=backoff_tagger)
-    print("Trigram tagger evaluation: ", round(backoff_tagger.accuracy(test_set)*100, 2), "%", "\n")
+    print("\nTagging accuracies with backoff:\n--------------------------------")
+    curr_tagger = None
+    for tagger in taggers:
+        curr_tagger = run_tagger(tagger, training_set, test_set, curr_tagger)
 
-    # 2.4
-    # The reason for this is that the taggers without a backoff model are not able to tag words that are not in the
-    # training set. This is because they do not have a fallback option. The backoff model is able to tag words that
-    # are not in the training set because it has a fallback option. The backoff model is able to use the previous
-    # tagger to tag words that are not in the training set. This is why the backoff model has a higher accuracy than
-    # the taggers without a backoff model.
-    # The reason why the BigramTagger without a backoff model has a lower accuracy than the corresponding tagger with
-    # a backoff model is because the BigramTagger without a backoff model is not able to tag words that are not in the
-    # training set.
+    # 2.4 --> read report
 
     # 2.5
     sentences_to_tag = []
@@ -53,24 +65,7 @@ def main():
         words_in_sentence = [word_info[0] for word_info in sentence]
         sentences_to_tag.append(words_in_sentence)
     default_tagger_results = nltk.pos_tag_sents(sentences_to_tag)
-
-    def evaluate(results, expected):
-        """
-        For each sentence in the test set, this function compares the results of the default tagger with the expected
-        :param results: The results of the default tagger
-        :param expected: The expected results
-        :return: The accuracy of the default tagger
-        """
-        correct = 0
-        total = 0
-        for i in range(len(results)):
-            for j in range(len(results[i])):
-                if results[i][j][1] == expected[i][j][1]:
-                    correct += 1
-                total += 1
-        return correct / total
-
-    print("Accuracy of the default tagger in NLTK: ", round(evaluate(default_tagger_results, test_set)*100, 2), "%")
+    print("\nAccuracy of the default tagger in NLTK: ", round(evaluate(default_tagger_results, test_set) * 100, 2), "%")
 
 
 main()
